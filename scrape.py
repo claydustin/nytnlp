9046e3dbaeac108e7ea8391781b229c958c05351
claydustin <clay.m.dustin@gmail.com>
2017-12-15 15:21:18 -0500

    added further stages to the parsing

--- scrape.py
+++ scrape.py
@@ -2,12 +2,19 @@ import ssl
 from urllib2 import urlopen
 from bs4 import BeautifulSoup
 
-
+  #takes URL and returns the NYT article body
   def scrape(url):
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
 
     html = urlopen(url,context=ctx)
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, 'html.parser')
+    ps = soup.find_all('p', class_='story-body-text story-content')
+    article = ''
+    for p in ps:
+      article += p
+    return article
+    
+      
     
