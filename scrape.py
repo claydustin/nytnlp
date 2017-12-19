eddea0eeaf021c6c7705e6b4690944d9b22f6d5a
claydustin <clay.m.dustin@gmail.com>
2017-12-19 11:54:49 -0500

    created class structure/added word2vec and doc2vec methods

--- scrape.py
+++ scrape.py
@@ -1,6 +1,9 @@
 import ssl
 from urllib2 import urlopen
 from bs4 import BeautifulSoup
+import nltk
+from nltk.corpus import stopwords
+import nltk.WordNetLemmatizer as wln
 
   #takes URL and returns the NYT article body
   def scrape(url):
@@ -16,5 +19,28 @@ from bs4 import BeautifulSoup
       article += p
     return article
     
-      
+  def tokenize(text, stop_file):
+    words = nltk.tokenize(text)
+    
+    #code to look for stop_file first in working directory and then in a custom path
+    #cust_words = file.read()
+    stop_words = cust_words | stopwords.words('english')
+    
+    #removes stop words
+    words = [word for word in words not in stop_words]
+    
+    #removes punctuation
+    words = [word for word in words if len(word)>1]
+    
+    #lemmatizes
+    words = [wln().lemmatize(word) for word in words]
+    
+    return words
+    
+  def guess_subject(words):
+    
+  def get_docRep(sentences):
+    
+   
+    
     
